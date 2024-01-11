aa24f0e3435c0043b859db8444d651939920148b
piyushXP0067 <114660663+piyushXP0067@users.noreply.github.com>
2024-01-11 12:55:18 +0530

    Initial commit

---
+++ lib/Views/contact_me.dart
@@ -0,0 +1,234 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import '../HelperClass/helper_class.dart';
+import '../globals/app_buttons.dart';
+import '../globals/app_colors.dart';
+import '../globals/app_text_style.dart';
+import '../globals/constants.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+
+class ContactUs extends StatelessWidget {
+  const ContactUs(
+      {Key? key}) : super(key: key);
+
+  void sendEmail(String name) async {
+    final Uri emailLaunchUri = Uri(
+      scheme: 'mailto',
+      path: 'piyushmishra.p@gmail.com',
+      queryParameters: {
+        'subject': 'Message from $name',
+        'body': 'Hello, I would like to get in touch with you!',
+      },
+    );
+
+    if (await canLaunch(emailLaunchUri.toString())) {
+      await launch(emailLaunchUri.toString());
+    } else {
+      print('Could not launch email client');
+    }
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return HelperClass(
+      mobile: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          buildContactText(),
+          Constants.sizedBox(height: 40.0),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.transparent,
+            elevation: 8,
+            child: TextField(
+              cursorColor: AppColors.white,
+              style: AppTextStyles.normalStyle(),
+              decoration: buildInputDecoration(hintText: 'Full Name'),
+            ),
+          ),
+          Constants.sizedBox(height: 20.0),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.transparent,
+            elevation: 8,
+            child: TextField(
+              cursorColor: AppColors.white,
+              style: AppTextStyles.normalStyle(),
+              decoration: buildInputDecoration(hintText: 'Email Address'),
+            ),
+          ),
+          Constants.sizedBox(height: 20.0),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.transparent,
+            elevation: 8,
+            child: TextField(
+              cursorColor: AppColors.white,
+              style: AppTextStyles.normalStyle(),
+              decoration: buildInputDecoration(hintText: 'Mobile Number'),
+            ),
+          ),
+          Constants.sizedBox(height: 20.0),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.transparent,
+            elevation: 8,
+            child: TextField(
+              cursorColor: AppColors.white,
+              style: AppTextStyles.normalStyle(),
+              decoration: buildInputDecoration(hintText: 'Email Subject'),
+            ),
+          ),
+          Constants.sizedBox(height: 20.0),
+          Material(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.transparent,
+            elevation: 8,
+            child: TextField(
+              maxLines: 15,
+              cursorColor: AppColors.white,
+              style: AppTextStyles.normalStyle(),
+              decoration: buildInputDecoration(hintText: 'Your Message'),
+            ),
+          ),
+          Constants.sizedBox(height: 40.0),
+          AppButtons.buildMaterialButton(
+              buttonName: 'Send Message', onTap: () {}),
+          Constants.sizedBox(height: 30.0),
+        ],
+      ),
+      tablet: buildForm(),
+      desktop: buildForm(),
+      paddingWidth: size.width * 0.2,
+      bgColor: AppColors.bgColor,
+    );
+  }
+
+
+
+  Column buildForm() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        buildContactText(),
+        Constants.sizedBox(height: 40.0),
+        Row(
+          children: [
+            Expanded(
+              child: Material(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.transparent,
+                elevation: 8,
+                child: TextField(
+                  cursorColor: AppColors.white,
+                  style: AppTextStyles.normalStyle(),
+                  decoration: buildInputDecoration(hintText: 'Full Name'),
+                ),
+              ),
+            ),
+            Constants.sizedBox(width: 20.0),
+            Expanded(
+              child: Material(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.transparent,
+                elevation: 8,
+                child: TextField(
+                  cursorColor: AppColors.white,
+                  style: AppTextStyles.normalStyle(),
+                  decoration: buildInputDecoration(hintText: 'Email Address'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Constants.sizedBox(height: 20.0),
+        Row(
+          children: [
+            Expanded(
+              child: Material(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.transparent,
+                elevation: 8,
+                child: TextField(
+                  cursorColor: AppColors.white,
+                  style: AppTextStyles.normalStyle(),
+                  decoration: buildInputDecoration(hintText: 'Mobile Number'),
+                ),
+              ),
+            ),
+            Constants.sizedBox(width: 20.0),
+            Expanded(
+              child: Material(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.transparent,
+                elevation: 8,
+                child: TextField(
+                  cursorColor: AppColors.white,
+                  style: AppTextStyles.normalStyle(),
+                  decoration: buildInputDecoration(hintText: 'Email Subject'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        Constants.sizedBox(height: 20.0),
+        Material(
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.transparent,
+          elevation: 8,
+          child: TextField(
+            maxLines: 15,
+            cursorColor: AppColors.white,
+            style: AppTextStyles.normalStyle(),
+            decoration: buildInputDecoration(hintText: 'Your Message'),
+          ),
+        ),
+        Constants.sizedBox(height: 40.0),
+        AppButtons.buildMaterialButton(
+            buttonName: 'Send Message', onTap: () {sendEmail("name");}),
+        Constants.sizedBox(height: 30.0),
+      ],
+    );
+  }
+
+  FadeInDown buildContactText() {
+    return FadeInDown(
+      duration: const Duration(milliseconds: 1200),
+      child: RichText(
+        text: TextSpan(
+          text: 'Contact ',
+          style: AppTextStyles.headingStyles(fontSize: 30.0),
+          children: [
+            TextSpan(
+              text: 'Me!',
+              style: AppTextStyles.headingStyles(
+                  fontSize: 30, color: AppColors.robinEdgeBlue),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  InputDecoration buildInputDecoration({required String hintText}) {
+    return InputDecoration(
+        hintText: hintText,
+        hintStyle: AppTextStyles.comfortaaStyle(),
+        enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none),
+        focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none),
+        filled: true,
+        fillColor: AppColors.bgColor2,
+        contentPadding:
+        const EdgeInsets.symmetric(horizontal: 24, vertical: 16));
+  }
+
+}
